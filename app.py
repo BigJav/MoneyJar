51f6b312ef285f406f69cd1a69a7050c7a2c9502
BigJav <134456195+BigJav@users.noreply.github.com>
2023-05-23 17:03:50 -0700

    Reverted app.py
    Removed transaction history feature

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 import streamlit as st
-import pandas as pd
-import base64
 
 class MoneyJar:
     def __init__(self, income):
@@ -21,18 +19,15 @@ class MoneyJar:
             "Giving": 0.1,
             "Personal Development": 0.1
         }
-        self.transaction_history = pd.DataFrame(columns=['Jar', 'Transaction Type', 'Amount'])
 
     def add_income(self, income):
         self.income += income
         for jar in self.jars:
             self.jars[jar] += income * self.percentages[jar]
-            self.transaction_history = self.transaction_history.append({'Jar': jar, 'Transaction Type': 'Deposit', 'Amount': income * self.percentages[jar]}, ignore_index=True)
 
     def withdraw(self, jar, amount):
         if self.jars[jar] >= amount:
             self.jars[jar] -= amount
-            self.transaction_history = self.transaction_history.append({'Jar': jar, 'Transaction Type': 'Withdrawal', 'Amount': amount}, ignore_index=True)
             return amount
         else:
             st.write("Insufficient funds in", jar)
@@ -41,15 +36,6 @@ class MoneyJar:
     def display_jars(self):
         for jar in self.jars:
             st.write(jar, ":", self.jars[jar])
-
-    def save_to_csv(self):
-        self.transaction_history.to_csv('transaction_history.csv', index=False)
-
-def get_table_download_link(df):
-    csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()
-    href = f'<a href="data:file/csv;base64,{b64}" download="transaction_history.csv">Download CSV File</a>'
-    return href
 
 # Streamlit UI
 st.title('Money Jar System')
@@ -71,10 +57,5 @@ if st.button('Withdraw'):
     st.write('Withdrawn!')
 
 # Display jars
-if st.button('Display Jars'):
-    my_jars.display_jars()
-
-# Save to CSV and provide download link
-if st.button('Save Transaction History'):
-    my_jars.save_to_csv()
-    st.markdown(get_table_download_link(my_jars.transaction_history), unsafe_allow_html=True)
+st.button('Display Jars')
+my_jars.display_jars()
